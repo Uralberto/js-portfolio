9a66415d5f693100d6ccf6c31bd60c321dd238c5
Uralberto <urievalo@hotmail.com>
2022-08-03 15:17:32 -0500

    subiendo la carpeta dist a GitHub

---
+++ dist/main.2e25f318d26d0eb8113c.js
@@ -0,0 +1,35 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => {
+	// webpackBootstrap
+	/******/ var __webpack_modules__ = {
+		/***/ './src/index.js':
+			/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+			/***/ () => {
+				eval(
+					"throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\src\\\\index.js: Unterminated string constant. (2:7)\\n\\n\\u001b[0m \\u001b[90m 1 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mTemplate\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m'@templates/Template.js'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 2 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[32m'@styles/main.css;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m        \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 3 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[32m'@styles/vars.styl'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m (\\u001b[36masync\\u001b[39m \\u001b[36mfunction\\u001b[39m \\u001b[33mApp\\u001b[39m() {\\u001b[0m\\n    at instantiate (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:72:32)\\n    at constructor (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:359:12)\\n    at Parser.raise (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3339:19)\\n    at Parser.readString (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3046:20)\\n    at Parser.getTokenFromCode (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:2621:14)\\n    at Parser.nextToken (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:2023:10)\\n    at Parser.next (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1927:10)\\n    at Parser.parseStatementContent (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14749:16)\\n    at Parser.parseStatement (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14643:17)\\n    at Parser.parseBlockOrModuleBlockBody (C:\\\\Users\\\\Windows 10\\\\Desktop\\\\DEV\\\\curso-de-webpack\\\\js-portfolio\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:15286:25)\");\n\n//# sourceURL=webpack://js-portfolio/./src/index.js?"
+				);
+
+				/***/
+			},
+
+		/******/
+	};
+	/************************************************************************/
+	/******/
+	/******/ // startup
+	/******/ // Load entry module and return exports
+	/******/ // This entry module doesn't tell about it's top-level declarations so it can't be inlined
+	/******/ var __webpack_exports__ = {};
+	/******/ __webpack_modules__['./src/index.js']();
+	/******/
+	/******/
+})();
